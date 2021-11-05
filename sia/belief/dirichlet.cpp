3ea813ae4078fb6d5f51dfc1ba67d1a58d8df307
Parker Owan <parker.owan@gmail.com>
2021-11-05 16:57:36 -0700

    Adds dirichlet distribution

---
+++ sia/belief/dirichlet.cpp
@@ -0,0 +1,108 @@
+/// Copyright (c) 2018-2021, Parker Owan.  All rights reserved.
+/// Licensed under BSD-3 Clause, https://opensource.org/licenses/BSD-3-Clause
+
+#include "sia/belief/dirichlet.h"
+#include "sia/common/exception.h"
+
+#include <glog/logging.h>
+#include <cmath>
+
+namespace sia {
+
+Dirichlet::Dirichlet(std::size_t dimension)
+    : Distribution(Generator::instance()) {
+  SIA_EXCEPTION(dimension >= 2, "Dirichlet distribution requires dim >= 2");
+  setAlpha(Eigen::VectorXd::Ones(dimension));
+}
+
+Dirichlet::Dirichlet(double alpha, double beta) : Dirichlet(2) {
+  setAlpha(Eigen::Vector2d{alpha, beta});
+}
+
+Dirichlet::Dirichlet(const Eigen::VectorXd& alpha) : Dirichlet(alpha.size()) {
+  setAlpha(m_alpha);
+}
+
+std::size_t Dirichlet::dimension() const {
+  return m_alpha.size();
+}
+
+const Eigen::VectorXd Dirichlet::sample() {
+  assert(m_gamma_dists.size() == dimension());
+
+  // Sample from K independent gamma distributions
+  Eigen::VectorXd x = Eigen::VectorXd::Zero(dimension());
+  for (std::size_t i = 0; i < dimension(); ++i) {
+    x(i) = m_gamma_dists[i](m_rng);
+  }
+  return x / x.sum();
+}
+
+double Dirichlet::logProb(const Eigen::VectorXd& x) const {
+  // Check if x is in the domain of the distribution
+  for (std::size_t i = 0; i < dimension(); i++) {
+    if ((x(i) > 1.0) || (x(i) < 0.0)) {
+      return -INFINITY;
+    }
+  }
+
+  // See: Sec 3 http://jonathan-huang.org/research/dirichlet/dirichlet.pdf
+  double a = log(tgamma(m_alpha.sum()));
+  double b = 0;
+  for (std::size_t i = 0; i < dimension(); ++i) {
+    b -= log(tgamma(m_alpha(i)));
+  }
+  double c = ((m_alpha.array() - 1.0) * x.array().log()).sum();
+  return a + b + c;
+}
+
+const Eigen::VectorXd Dirichlet::mean() const {
+  return m_alpha / m_alpha.sum();
+}
+
+const Eigen::VectorXd Dirichlet::mode() const {
+  double min_alpha = m_alpha.minCoeff();
+  if (min_alpha <= 1.0) {
+    LOG(WARNING) << "Mode is invalid for alpha concentrations <= 1";
+  }
+  return (m_alpha.array() - 1.0) / (m_alpha.sum() - double(m_alpha.size()));
+}
+
+const Eigen::MatrixXd Dirichlet::covariance() const {
+  double a0 = m_alpha.sum();
+  double den = pow(a0, 2) * (a0 + 1);
+  Eigen::MatrixXd cov = -m_alpha.transpose() * m_alpha / den;
+  cov.diagonal() = m_alpha.array() * (a0 - m_alpha.array()) / den;
+  return cov;
+}
+
+const Eigen::VectorXd Dirichlet::vectorize() const {
+  Eigen::VectorXd data = m_alpha;
+  return data;
+}
+
+bool Dirichlet::devectorize(const Eigen::VectorXd& data) {
+  std::size_t n = dimension();
+  std::size_t d = data.size();
+  if (d != n) {
+    LOG(WARNING) << "Devectorization failed, expected vector size " << n
+                 << ", received " << d;
+    return false;
+  }
+  setAlpha(data);
+  return true;
+}
+
+const Eigen::VectorXd& Dirichlet::alpha() const {
+  return m_alpha;
+}
+
+void Dirichlet::setAlpha(const Eigen::VectorXd& alpha) {
+  m_alpha = alpha;
+  m_gamma_dists.clear();
+  for (std::size_t i = 0; i < dimension(); ++i) {
+    m_gamma_dists.emplace_back(std::gamma_distribution<double>(alpha(i), 1));
+  }
+}
+
+}  // namespace sia
---
+++ sia/belief/dirichlet.h
@@ -0,0 +1,43 @@
+/// Copyright (c) 2018-2021, Parker Owan.  All rights reserved.
+/// Licensed under BSD-3 Clause, https://opensource.org/licenses/BSD-3-Clause
+
+#pragma once
+
+#include "sia/belief/distribution.h"
+
+#include <Eigen/Dense>
+
+namespace sia {
+
+/// Dirichlet distribution defined by concentrations alpha > 0.  Note for a
+/// sample x outside of the distribution support [0, 1], the logProb returns
+/// -INFINITY
+class Dirichlet : public Distribution {
+ public:
+  /// Creates a default multivariate Dirichlet (alpha = 1).
+  explicit Dirichlet(std::size_t dimension);
+
+  /// Creates a beta distribution from two concentrations (2D Dirichlet).
+  explicit Dirichlet(double alpha, double beta);
+
+  /// Creates a multivariate Dirichlet from vector of concentrations.
+  explicit Dirichlet(const Eigen::VectorXd& alpha);
+  virtual ~Dirichlet() = default;
+  std::size_t dimension() const override;
+  const Eigen::VectorXd sample() override;
+  double logProb(const Eigen::VectorXd& x) const override;
+  const Eigen::VectorXd mean() const override;
+  const Eigen::VectorXd mode() const override;
+  const Eigen::MatrixXd covariance() const override;
+  const Eigen::VectorXd vectorize() const override;
+  bool devectorize(const Eigen::VectorXd& data) override;
+
+  const Eigen::VectorXd& alpha() const;
+  void setAlpha(const Eigen::VectorXd& alpha);
+
+ private:
+  Eigen::VectorXd m_alpha;
+  std::vector<std::gamma_distribution<double>> m_gamma_dists;
+};
+
+}  // namespace sia
