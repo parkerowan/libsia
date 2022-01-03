0d9c7b35d1dee8da28d1c1f68b54783af118a72c
Parker Owan <parker.owan@gmail.com>
2022-01-03 00:33:33 -0800

    Update copyright header

--- python/py_models.cpp
+++ python/py_models.cpp
@@ -1,4 +1,4 @@
-/// Copyright (c) 2018-2021, Parker Owan.  All rights reserved.
+/// Copyright (c) 2018-2022, Parker Owan.  All rights reserved.
 /// Licensed under BSD-3 Clause, https://opensource.org/licenses/BSD-3-Clause
 
 #include "python/py_models.h"
