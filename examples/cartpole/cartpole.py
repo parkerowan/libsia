e5551d6c62476a0017797d70e30ed7b846954357
Parker Owan <parker.owan@gmail.com>
2022-04-08 22:58:47 -0700

    Adds confidence ellipse for GMR/GMM

--- examples/cartpole/cartpole.py
+++ examples/cartpole/cartpole.py
@@ -8,6 +8,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FFMpegWriter
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
 import seaborn as sns
 import ast  # Because argparse bool doesn't work
 sns.set_theme(style="whitegrid")
@@ -104,6 +106,56 @@ def plot_cartpole_trajectory(datafile: str, animate: bool, trace: bool,
                 plt.pause(0.001)
 
                 writer.grab_frame()
+                
+
+def plot_confidence_ellipse(mu, cov, ax, n_std=3.0, facecolor='none', **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    cov : array-like, shape (n, n)
+        Covariance matrix.
+
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    **kwargs
+        Forwarded to `~matplotlib.patches.Ellipse`
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+    """
+
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
+                      facecolor=facecolor, **kwargs)
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = mu[0]
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = mu[1]
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
 
 
 if __name__ == "__main__":
