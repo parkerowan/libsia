e41cf624321199d0cad1e7b75866ca3be64989ba
Parker Owan <parker.owan@gmail.com>
2022-04-08 23:51:57 -0700

    Adds pages to gitlab-ci

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ copyright = '2018-2022, Parker Owan'
 author = 'Parker Owan'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.1'
 
 # -- General configuration ---------------------------------------------------
 
