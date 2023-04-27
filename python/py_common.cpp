9fae49bfed8c30e07d4597fe58706e42ea9b08e3
Parker Owan <parker.owan@gmail.com>
2023-04-26 21:44:47 -0700

    Remove deprecated python bindings

--- python/py_common.cpp
+++ python/py_common.cpp
@@ -7,17 +7,7 @@
 void export_py_common(py::module& m_sup) {
   py::module m = m_sup;
 
-  py::class_<sia::LoggerInterface, PyLoggerInterface>(m, "LoggerInterface")
-      .def(py::init<>())
-      .def("debug", &sia::LoggerInterface::debug, py::arg("msg"))
-      .def("info", &sia::LoggerInterface::info, py::arg("msg"))
-      .def("warn", &sia::LoggerInterface::warn, py::arg("msg"))
-      .def("error", &sia::LoggerInterface::error, py::arg("msg"))
-      .def("critical", &sia::LoggerInterface::critical, py::arg("msg"));
-
   py::class_<sia::Logger>(m, "Logger")
-      .def_static("setCustomLogger", &sia::Logger::setCustomLogger,
-                  py::arg("interface"))
       .def_static("debug", &sia::Logger::debug, py::arg("msg"))
       .def_static("info", &sia::Logger::info, py::arg("msg"))
       .def_static("warn", &sia::Logger::warn, py::arg("msg"))
